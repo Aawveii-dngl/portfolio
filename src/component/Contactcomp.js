af885c24c792ec14534f78a9864171a42a9a303b
Avi Dangol <dangolavi9999@gmail.com>
2020-12-14 23:37:06 +0545

    Form1

--- src/component/Contactcomp.js
+++ src/component/Contactcomp.js
@@ -1,45 +1,54 @@
-import {React,Component, StrictMode} from 'react';
-import {Button,Form,Container,Row,Col, Alert } from 'react-bootstrap';
+import React,{Component} from 'react';
+import {Button,Container,Row,Col} from 'react-bootstrap';
 import {FaGithub, FaLinkedin,FaYoutube } from "react-icons/fa";
 import { IconContext } from 'react-icons';
+import {Control,LocalForm,Errors} from 'react-redux-form';
+
+const required = (val) => val && val.length;
+const maxLength = (len) =>(val) => !(val) || (val.length <=len);
+const minLength = (len) =>(val) => (val) && (val.length >=len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+const Checked = (val) => val;
+
 
 export default class Contactcomp extends Component{
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-            name:'',
-            email:'',
-            message:''
+    
+        this.state = {
+             agree:'false',
+             password:'',
+             confpassword:''
         }
     }
-    Handlename = event => {
-        this.setState({
-            name:event.target.value
-        });
+
+    Clicked = () =>{
+        this.setState({agree:!this.state.agree})
+        console.log(this.state.agree)
     }
-    handlerEmail = (event)=> {
-        this.setState({
-            email:event.target.value
-        });
+
+    Pass = (e) =>{
+        this.setState({password:e.target.value})
+        console.log(this.state.password)
     }
-    Handlemessage = event => {
-        this.setState({
-            message:event.target.value
-        });
-    }
-    Handlesubmit = event =>{
-        console.log(this.state);
-        alert(`${this.state.name} ${this.state.email} ${this.state.message}`);
-        event.preventDefault();
+
+    confPass = (event) => {
+         if (event.target.value !== this.state.password) { 
+             <p>Error</p>
+              this.setState({confpassword: event.target.value}) } }
+    
+    handlerSubmit(values){
+        console.log(JSON.stringify(values));
+        alert(JSON.stringify(values));
     }
 
     render(){
     return(
         <Container>
-        
         <Row>
             <Col>
-            <div class = "p-5">
+            <div className = "p-5">
             <h1>GET IN TOUCH</h1>
             <h4>Hey we are looking forward to start a project with you</h4>
             <p>The term déjà vu is French and means, literally, "already seen." Those who have experienced the feeling describe it as an overwhelming sense of familiarity with something that shouldn't be familiar at all. Say, for example, you are traveling to England for the first time</p>
@@ -53,23 +62,131 @@ export default class Contactcomp extends Component{
             </div>
             </Col>
             <Col>
-            <div class = "mt-5 p-5 ">
-            <Form class = "p-5 " onSubmit = {this.Handlesubmit}>
-            <Form.Group >
-                <Form.Control type="text" placeholder="Enter your Name" value = {this.state.name} onChange = {this.Handlename}/>
-            </Form.Group>
+            <div className = "mt-5 p-5 ">
+            <LocalForm className = "p-5 " onSubmit = {(values) => this.handlerSubmit(values)}>
+                <Row className = "form-group" >
+                    <Col>
+                        <Control.text model = ".name"
+                        name = "name"
+                        className = "form-control"
+                        placeholder="Enter your Name"
+                        validators = {{
+                            required, minLength:minLength(3), maxLength:maxLength(15)
+                        }} 
+                        />
+                        <Errors
+                            className="text-danger"
+                            model=".name"
+                            show="touched"
+                            messages= {{
+                                required:'Required',
+                                minLength:'Must be greater than two characters',
+                                maxLength:'Must be less tham 15 characters'
+                            }}
+                        />
+                    </Col>
+                </Row>
 
-            <Form.Group controlId="formBasicEmail">
-                <Form.Control type="email" placeholder="Enter a valid email address" value = {this.state.email} onChange = {this.handlerEmail}/>
-            </Form.Group>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-            <Form.Control type="textarea" rows={3}  placeholder="Enter your message" value = {this.state.message} onChange = {this.Handlemessage}/>
-            </Form.Group>
-            <Form.Check type="checkbox" className="my-1 mr-sm-2 a" label="I accept the Terms of Service"/>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-            </Form>
+                <Row className = "form-group" >
+                    <Col>
+                        <Control.text model = ".email"
+                        name="email" 
+                        className = "form-control"
+                        placeholder="Enter a valid email address"
+                        validators = {{
+                            required,validEmail
+                        }}
+                        />
+                        <Errors
+                        className="text-danger"
+                        model=".email"
+                        show="touched"
+                        messages= {{
+                            required:'Required',
+                            validEmail:'Use a proper email address'
+                        }}
+                        />  
+                    </Col>
+                </Row>
+                <Row className = "form-group" >
+                    <Col>
+                        <Control.text model = ".password"
+                        name="password" 
+                        className = "form-control"
+                        placeholder="Enter password"
+                        onChange = {this.Pass}
+                        validators = {{
+                            required
+                        }}
+                        />
+                        <Errors
+                        className="text-danger"
+                        model=".password"
+                        show="touched"
+                        messages= {{
+                            required:'Required'
+                        }}
+                        />  
+                    </Col>
+                </Row>
+                <Row className = "form-group" >
+                <Col>
+                    <Control.text model = ".confpassword"
+                    name="confpassword" 
+                    className = "form-control"
+                    placeholder="Enter password again"
+                    onChange = {this.confPass}
+                    validators = {{
+                        required
+                    }}
+                    />
+                    <Errors
+                    className="text-danger"
+                    model=".confpassword"
+                    show="touched"
+                    messages= {{
+                        required:'Required',
+                    }}
+                    />  
+                </Col>
+                </Row>
+                <Row className = "form-group" >
+                    <Col>
+                    <Control.textarea model = ".message"
+                        name = "message"
+                        className = "form-control"
+                        placeholder="Enter your message"/>
+                    </Col>
+                </Row>
+                <Row className = "form-group">
+                    <Col>
+                    <Control.checkbox model =".agree"
+                        name = "agree" 
+                        onClick = {this.Clicked}
+                        className = "form-check-input"
+                        validators = {{
+                            Checked
+                        }}
+                    />
+                    {' '}
+                    <strong>i accept terms and services</strong>
+                    <Errors
+                    className="text-danger"
+                    model=".agree"
+                    
+                    messages= {{
+                        Checked:'Please accept our condition'
+                    }}
+                    /> 
+                    </Col>
+                </Row>
+                <Button 
+                    variant="primary" 
+                    type="submit"
+                    disabled={this.state.agree}>
+                    Submit
+                </Button>
+            </LocalForm>
             </div>
             </Col>
         </Row>
